9a520d06df79578ed7dead7bf2770e15ca38d839
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2025-09-08 19:09:42 -0300

    Criando AbrigoService

--- src/main/java/br/com/alura/AdopetConsoleApplication.java
+++ src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -14,10 +14,15 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import br.com.alura.service.AbrigoService;
+
 public class AdopetConsoleApplication {
 
     public static void main(String[] args) {
         AdopetConsoleApplication adopetConsoleApplication = new AdopetConsoleApplication();
+
+        AbrigoService abrigoService = new AbrigoService();
+
         System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
         try {
             int opcaoEscolhida = 0;
@@ -33,9 +38,9 @@ public class AdopetConsoleApplication {
                 opcaoEscolhida = Integer.parseInt(textoDigitado);
 
                 if (opcaoEscolhida == 1) {
-                    adopetConsoleApplication.listarAbrigo();
+                    abrigoService.listarAbrigo();
                 } else if (opcaoEscolhida == 2) {
-                    adopetConsoleApplication.cadastrarAbrigo();
+                    abrigoService.cadastrarAbrigo();
                 } else if (opcaoEscolhida == 3) {
                     adopetConsoleApplication.listarPets();
                 } else if (opcaoEscolhida == 4) {
@@ -50,48 +55,6 @@ public class AdopetConsoleApplication {
             System.out.println("Finalizando o programa...");
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    public void listarAbrigo() throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        String uri = "http://localhost:8080/abrigos";
-        HttpResponse<String> response = montarRequisicaoGet(uri, client);
-        String responseBody = response.body();
-        JsonArray jsonArray = JsonParser.parseString(responseBody).getAsJsonArray();
-        System.out.println("Abrigos cadastrados:");
-        for (JsonElement element : jsonArray) {
-            JsonObject jsonObject = element.getAsJsonObject();
-            long id = jsonObject.get("id").getAsLong();
-            String nome = jsonObject.get("nome").getAsString();
-            System.out.println(id + " - " + nome);
-        }
-    }
-
-    public void cadastrarAbrigo() throws IOException, InterruptedException {
-        System.out.println("Digite o nome do abrigo:");
-        String nome = new Scanner(System.in).nextLine();
-        System.out.println("Digite o telefone do abrigo:");
-        String telefone = new Scanner(System.in).nextLine();
-        System.out.println("Digite o email do abrigo:");
-        String email = new Scanner(System.in).nextLine();
-
-        JsonObject json = new JsonObject();
-        json.addProperty("nome", nome);
-        json.addProperty("telefone", telefone);
-        json.addProperty("email", email);
-
-        HttpClient client = HttpClient.newHttpClient();
-        String uri = "http://localhost:8080/abrigos";
-        HttpResponse<String> response = montarRequisicaoPost(uri, client, json);
-        int statusCode = response.statusCode();
-        String responseBody = response.body();
-        if (statusCode == 200) {
-            System.out.println("Abrigo cadastrado com sucesso!");
-            System.out.println(responseBody);
-        } else if (statusCode == 400 || statusCode == 500) {
-            System.out.println("Erro ao cadastrar o abrigo:");
-            System.out.println(responseBody);
         }
     }
 
